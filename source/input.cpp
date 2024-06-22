2196f851529b290d8fd185c61b724f2489b61726
Brandon Little <blittle@inco.cc>
2024-06-22 00:21:14 -0400

    Only call hooks if port input exists

--- source/input.cpp
+++ source/input.cpp
@@ -37,21 +37,21 @@ LUA_FUNCTION(IsInputPortOpen)
 {
     const auto port = (unsigned int)LUA->CheckNumber(1);
 
-    if (portInput.count(port) != 0) {
-        const auto input = portInput.at(port);
-        const auto open = input->isPortOpen();
+    if (portInput.count(port) > 0) {
+        auto input = portInput.at(port);
 
-        LUA->PushBool(open);
+        LUA->PushBool(input->isPortOpen());
 
-        if (not open) {
+        if (not LUA->GetBool()) {
             portInput.erase(port);
 
             delete input;
         }
+
+        return 1;
     }
-    else {
-        LUA->PushBool(false);
-    }
+
+    LUA->PushBool(false);
 
     return 1;
 }
@@ -60,42 +60,42 @@ LUA_FUNCTION(CloseInputPort)
 {
     const auto port = (unsigned int)LUA->CheckNumber(1);
 
-    LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
-    LUA->GetField(-1, "hook");
-    LUA->GetField(-1, "Run");
-    LUA->PushString("ShouldCloseMIDIInputPort");
-    LUA->PushNumber(port);
-    LUA->Call(2, 1);
-
-    const auto should = LUA->GetBool(-1) or LUA->IsType(-1, GarrysMod::Lua::Type::Nil);
-
-    if (should and portInput.count(port) != 0) {
-              auto input = portInput.at(port);
-        const auto open = input->isPortOpen();
-
-        LUA->PushBool(open);
-
-        if (open) {
-            try {
-                input->closePort();
-            }
-            catch (RtMidiError &error) {
-                LUA->ThrowError(error.what());
-            }
-        }
-
-        portInput.erase(port);
-
-        delete input;
-
-        LUA->GetField(-2, "Run");
-        LUA->PushString("OnMIDIInputPortClosed");
+    if (portInput.count(port) > 0) {
+        LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+        LUA->GetField(-1, "hook");
+        LUA->GetField(-1, "Run");
+        LUA->PushString("ShouldCloseMIDIInputPort");
         LUA->PushNumber(port);
-        LUA->Call(2, 0);
+        LUA->Call(2, 1);
+
+        if (LUA->GetBool() or LUA->IsType(-1, GarrysMod::Lua::Type::Nil)) {
+            auto input = portInput.at(port);
+
+            LUA->PushBool(input->isPortOpen());
+
+            if (LUA->GetBool()) {
+                try {
+                    input->closePort();
+                }
+                catch (RtMidiError &error) {
+                    LUA->ThrowError(error.what());
+                }
+            }
+
+            portInput.erase(port);
+
+            delete input;
+
+            LUA->GetField(-2, "Run");
+            LUA->PushString("OnMIDIInputPortClosed");
+            LUA->PushNumber(port);
+            LUA->Call(2, 0);
+
+            return 1;
+        }
     }
-    else {
-        LUA->PushBool(false);
-    }
+
+    LUA->PushBool(false);
 
     return 1;
 }
